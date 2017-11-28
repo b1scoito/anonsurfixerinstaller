140b34f45bfd6ac4037ac0cfc9d583a188781c73
Biscoitao <rafaelbadas123@gmail.com>
2017-11-28 04:00:55 +0100

    Update 2.0
    Ultra update

--- installptbr.sh
+++ installptbr.sh
@@ -8,62 +8,90 @@ Blue='\033[1;34m'
 Purple='\033[1;35m'
 Cyan='\033[1;36m'
 White='\033[1;37m'
+
+tor='/usr/bin/tor'
+anonsurf='/usr/bin/anonsurf'
+nscd='/usr/sbin/nscd'
+resolvconf='/usr/lib/resolvconf'
+dnsmasq='/usr/sbin/dnsmasq'
+nload='/usr/bin/nload'
+
 if [ $(id -u) != 0 ]
 then
 	echo
 	echo
-	echo -e "${Red} Você está bebado? Execute em modo ROOT!!"
+	echo -e "${Red} Você está bebado?? Execute em modo root!!"
 	echo
 	echo
 	exit
 else
 	echo
 fi
+sleep 0.4
 echo -e "${Red}Salves: V4p0r, D3z3n0v3, B33ck, Adolfinho, Bacon, all friends.."
-
+sleep 0.4
 echo -e "${Red}---------------------------------------------------------"
-
-echo -e "${Red}AnonSurf Resolvedor & Instalador - By: Biscoitao"
+sleep 0.4
+echo -e "${Red}AnonSurf Consertador & Instalador - By: Biscoitao"
 sleep 3.5
 echo
+sleep 10 &
+PID=$!
+i=1
+sp="/-\|"
+echo -n ' '
+while [ -d /proc/$PID ]
+do
+  printf "\b${sp:i++%${#sp}:1}"
+done
 echo
 echo
-echo
-echo
-echo
-echo
-echo
-echo
-echo
-echo
-echo
-echo
-echo
-echo
-echo
-echo
-echo
-echo
-echo
-echo
-echo
-echo
-echo -e "${Blue}"
-echo
-echo
-#Uninstall Tor
-sudo apt-get purge tor -y
+if [ -e $tor ]
+then
+    echo -e "${Red}Tor -- > Instalado"
+else
+    echo -e "${Red}Tor -- > Não achei, estarei fazendo o download para você!" && sudo apt-get install tor -y && sudo apt-get install tor-geoipdb -y
+fi
+sleep 6
+if [ -e $anonsurf ]
+then
+    echo -e "${Red}AnonSurf -- > Instalado"
+else
+    echo -e "${Red}AnonSurf -- > Não achei, estarei fazendo o download para você!" && sudo apt-get install anonsurf -y
+fi
+sleep 6
+if [ -e $nscd ]
+then
+    echo -e "${Red}Nscd -- > Instalado"
+else
+    echo -e "${Red}Nscd -- > Não achei, estarei fazendo o download para você!" && sudo apt-get install nscd -y
+fi
+sleep 6
+if [ -e $resolvconf ]
+then
+    echo -e "${Red}Resolvconf -- > Instalado"
+else
+    echo -e "${Red}Resolvconf -- > Não achei, estarei fazendo o download para você!" && sudo apt-get install resolvconf -y
+fi
+sleep 6
+if [ -e $dnsmasq ]
+then
+    echo -e "${Red}Dnsmasq -- > Instalado"
+else
+    echo -e "${Red}Dnsmasq -- > Não achei, estarei fazendo o download para você!" && sudo apt-get install dnsmasq -y
+fi
+sleep 6
+if [ -e $nload ]
+then
+    echo -e "${Red}Nload -- > Instalado"
+else
+    echo -e "${Red}Nload -- > Não achei, estarei fazendo o download para você!" && sudo apt-get install nload -y
+fi
+echo "Checamento completo!"
 clear
-#Install Tor
-sudo apt-get install tor -y
-clear
-#Install AnonSurf, Nload, tor-geoipdb
-sudo apt-get install anonsurf -y && sudo apt-get install nload -y && sudo apt-get install tor-geoipdb
-clear
-#Install Nscd, Resolvconf, Dnsmasq
-sudo apt-get install nscd -y && sudo apt-get install resolvconf -y && sudo apt-get install dnsmasq -y
-clear
+sleep 6
 #Fix Problems
+echo -e ${Cyan}
 sudo apt-get -f -y
 clear
 #Install Upgrades, Updates
@@ -74,48 +102,25 @@ sudo update-rc.d -f tor remove
 clear
 #Kill the tor process
 sudo pkill -x tor
-clear
 #Execute Tor
 sudo /usr/sbin/tor
-clear
 #Start AnonSurf, and show your ip
 sudo anonsurf start
 sudo anonsurf myip
 echo -e "${Cyan}AnonSurf Ativado!"
-sleep 3
+sleep 1
 echo -e "${Cyan}AnonSurf Consertado!"
-sleep 3
-sleep 10 &
-PID=$!
-i=1
-sp="/-\|"
-echo -n ' '
-while [ -d /proc/$PID ]
-do
-  printf "\b${sp:i++%${#sp}:1}"
-done
-echo -e "${Red}Tor -- > Instalado"
-sleep 6
-echo -e "${Red}AnonSurf -- > Instalado"
-sleep 6
-echo -e "${Red}Nscd -- > Instalado"
-sleep 6
-echo -e "${Red}Resolvconf -- > Instalado"
-sleep 6
-echo -e "${Red}Dnsmasq -- > Instalado"
-sleep 4
-echo "Completo!"
-sleep 2
-echo -e "${Purple}Todo trabalho foi feito agora vamos dançar com o ANONYMOUS!"
-sleep 4
+sleep 2.1
+echo -e "${Purple}Todo trabalho foi feito, agora vamos dançar com o anonymous!"
+sleep 3.2
 read -r -p "Você deseja parar o anonsurf agora? [dstat/y] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
-        sudo anonsurf stop && echo "Obrigado por usar adeus!" 
+        sudo anonsurf stop && echo -e "${Purple}Obrigado por usar, adeus."
         ;;
     *)
         sudo nload
         ;;
 esac
 
-echo ".----------------------------------------------."
+echo -e "${Purple}.----------------------------------------------."
